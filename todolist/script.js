e02e15a59fcee1ac63fdbc8985bc064100240cd9
joaogabrielfragosojardim <joaogabrielmgtsu@gmail.com>
2021-09-07 23:42:11 -0300

    ToDoList

--- todolist/script.js
+++ todolist/script.js
@@ -1,70 +1,86 @@
-let inputTasks = document.getElementById("containerInputTasks");
-
 let btnPlus = document.getElementById("btnPlus");
 
 btnPlus.addEventListener("click", (c) => {
   turnOn();
 });
 
-let bntX = document.getElementById("close");
+let divToAddTasks = document.getElementById("containerInputTasks");
 
-bntX.addEventListener("click", (c) => {
+function turnOn(c) {
+  divToAddTasks.style.display = "flex";
+}
+
+let btnX = document.getElementById("close");
+
+btnX.addEventListener("click", (c) => {
   turnOff();
 });
 
-function turnOn() {
-  inputTasks.style.display = "flex";
-  document.documentElement.style.overflow = "hidden";
-}
-
-function turnOff() {
-  inputTasks.style.display = "none";
-  document.documentElement.style.overflow = "auto";
+function turnOff(c) {
+  divToAddTasks.style.display = "none";
+  let taskName = document.getElementById("taskName");
+  let taskDescription = document.getElementById("Task");
+  taskName.value = "";
+  taskDescription.value = "";
 }
 
 let btnAddTask = document.getElementById("addTasks");
-
 btnAddTask.addEventListener("click", (e) => {
   e.preventDefault();
   let taskName = document.getElementById("taskName").value;
-  let task = document.getElementById("Task").value;
-  createTask(taskName, task);
-  turnOff();
+  let taskDescription = document.getElementById("Task").value;
+  addTasks(taskName, taskDescription);
 });
 
-let counter = 0;
+let tasks = [];
 
-function createTask(taskName, task) {
-  counter++;
-  let containerCards = document.getElementById("containerTasks");
-  let newTask = document.createElement("div");
-  newTask.classList.add("cards");
-  newTask.innerHTML =
-    " <div class='containerContent'" +
-    " idcard=" +
-    counter +
-    "><h2>" +
-    taskName +
-    "</h2><p>" +
-    task +
-    "</p></div><div class='containerButtons'><img src='/todolist/assets/trash.png'" +
-    " idimage=" +
-    counter +
-    "></div>";
-  containerCards.appendChild(newTask);
-  showCounter(counter);
+function idGenerator() {
+  let id = Date.now();
+
+  return id;
 }
 
-function showCounter(counter) {
-  let showCounter = document.getElementById("numTasks");
-  if (counter > 0) {
-    showCounter.innerHTML = `you have ${counter} thing to do`;
-  }
-  if (counter >= 2) {
-    showCounter.innerHTML = `you have ${counter} things to do`;
-  }
+function addTasks(taskName, taskDescription) {
+  var taskObject = {
+    taskName: taskName,
+    taskDescription: taskDescription,
+    taskTrash:
+      " <img src='/todolist/assets/trash.png'/ onclick='deleteTask(this)'>",
+    idRandom: idGenerator(),
+  };
+  tasks.push(taskObject);
+  turnOff();
+  updateTask();
 }
 
-function erase(){
+function updateTask() {
+  if (tasks.length == 0) {
+    containerTasks.innerHTML = "";
+  }
 
+  let div = "<div>";
+  tasks.forEach((task) => {
+    let containerTasks = document.getElementById("containerTasks");
+    div += ` <div class="cards" id-data="${task.idRandom}">
+            <div class="containerContent">
+              <h2>${task.taskName}</h2>
+              <p>${task.taskDescription}</p>
+            </div>
+            <div class="containerButtons">
+              <img src="/todolist/assets/trash.png" onclick="deleteTask(this)" id-data="${task.idRandom}" />
+            </div>
+          </div>`;
+    div += "</div>";
+    containerTasks.innerHTML = div;
+  });
+}
+
+function deleteTask(element) {
+  tasks.find((item, i) => {
+    if (item.idRandom == element.getAttribute(["id-data"])) {
+      return tasks.splice(i, 1);
+    }
+  });
+
+  updateTask();
 }
